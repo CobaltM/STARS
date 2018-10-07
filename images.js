84b6a464359f5c9b519b7b1297af87c4721c038e
hamedouldhamou <33500154+hamedouldhamou@users.noreply.github.com>
2018-10-07 01:11:16 -0400

    images function

---
+++ images.js
@@ -0,0 +1,111 @@
+ var x = JSON.stringify(p.id);
+      if (x === '{"id":"1"}')
+       {
+        document.getElementById("myImg").src="../images/1.jpg";
+
+       }
+       else if (x === '{"id":"2"}')
+       {
+        document.getElementById("myImg").src="../images/2.1.jpg";
+
+       }
+       else if (x === '{"id":"3"}')
+       {
+        document.getElementById("myImg").src="../images/3.1.jpg";
+
+       }
+       else if (x === '{"id":"4"}')
+       {
+        document.getElementById("myImg").src="../images/4.jpg";
+
+       }
+       else if (x === '{"id":"5"}')
+       {
+        document.getElementById("myImg").src="../images/5.jpg";
+
+       }
+       else if (x === '{"id":"6"}')
+       {
+        document.getElementById("myImg").src="../images/6.1.png";
+
+       }
+       else if (x === '{"id":"7"}')
+       {
+        document.getElementById("myImg").src="../images/7.1.jpg";
+
+       }
+       else if (x === '{"id":"8"}')
+       {
+        document.getElementById("myImg").src="../images/8.jpg";
+
+       }
+       else if (x === '{"id":"9"}')
+       {
+        document.getElementById("myImg").src="../images/9.png";
+
+       }
+       else if (x === '{"id":"10"}')
+       {
+        document.getElementById("myImg").src="../images/10.jpg";
+
+       }
+       else if (x === '{"id":"11"}')
+       {
+        document.getElementById("myImg").src="../images/11.jpg";
+
+       }
+       else if (x === '{"id":"12"}')
+       {
+        document.getElementById("myImg").src="../images/12.jpg";
+
+       }
+       else if (x === '{"id":"13"}')
+       {
+        document.getElementById("myImg").src="../images/13.1.jpg";
+
+       }
+       else if (x === '{"id":"14"}')
+       {
+        document.getElementById("myImg").src="../images/14.jpg";
+
+       }
+       else if (x === '{"id":"15"}')
+       {
+        document.getElementById("myImg").src="../images/15.png";
+
+       }
+       else if (x === '{"id":"16"}')
+       {
+        document.getElementById("myImg").src="../images/16.jpg";
+
+       }
+
+       else if (x === '{"id":"17"}')
+       {
+        document.getElementById("myImg").src="../images/17.png";
+
+       }
+
+       else if (x === '{"id":"18}')
+       {
+        document.getElementById("myImg").src="../images/18.png";
+
+       }
+
+       else if (x === '{"id":"19"}')
+       {
+        document.getElementById("myImg").src="../images/19.jpg";
+
+       }
+       else if (x === '{"id":"20"}')
+       {
+        document.getElementById("myImg").src="../images/20.jpg";
+
+       }
+       else if (x === '{"id":"21"}')
+       {
+        document.getElementById("myImg").src="../images/21.jpg";
+
+       }
+
+       <img alt = "hi" id="myImg" src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" height="150" width="150">
